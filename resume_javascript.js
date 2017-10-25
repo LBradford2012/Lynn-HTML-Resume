a1fce07b95a529c5004b33055b6325623426adf8
LBradford2012 <LBradford2012@gmail.com>
2017-10-25 16:19:48 -0400

    Lynn HTML project
    Resume project

---
+++ resume_javascript.js
@@ -0,0 +1,30 @@
+/*javascript class file*/
+var greet=prompt("What is your favorite color?")//
+var age;//
+//name="ABC"//
+//window.alert("Test")//
+//window.onload=alert("Welcome to my resume.")
+//var add = function (a, b) {
+ //   return a + b;
+//};//
+<button onclick=mxby>Test</button>
+//https://www.w3schools.com/js/js_operators.asp//
+//var x
+//var m
+//var y
+//var b 	
+window.alert("Sandwich")
+//var mxby=function(x,m,y,b){
+	//var x=prompt("x=")
+	//var m=prompt("m=")
+	//var y=prompt("y=")
+	//var b=prompt("b=")
+	//return x*m+y*b
+	//alert(x*m+y*b)}//
+//https:www.w3schools.com/js/js_events.asp//	
+
+//var greet=prompt{("Do you like the purple background: Y or N?");}
+//if (greet=="Y"):
+//	window.alert("I'm glad you like it.")
+//else:
+//	window.alert("Okay.")
